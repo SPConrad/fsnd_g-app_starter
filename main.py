aff99945aa393a174c673a9297ccf65398ee80b1
SPConrad <sean.conrad@willowtreeapps.com>
2017-07-30 19:28:53 -0400

    Made it a few more lessons in

--- main.py
+++ main.py
@@ -15,26 +15,97 @@
 import webapp2
 
 form = """
-<form method="post" action="/testform">
-	<input name="q">
+<form method="post">
+    What is your birthday?
+    
+	<label>
+        Month
+        <input type="text" name="month" value="%(month)s">
+    </label>
+    <label>
+        Day
+        <input type="text" name="day" value="%(day)s">
+    </label>
+    <label>
+        Year
+        <input type="text" name="year" value="%(year)s">
+    </label>
+    <div style="color: red">%(error)s</div>
+    <br>
+    <br>
 	<input type="submit">
 </form>
 """
 
 class MainPage(webapp2.RequestHandler):
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {
+            "error": error, 
+            "month": month, 
+            "year": year, 
+            "day": day 
+            })
+
     def get(self):
-        #self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(form)
+        self.write_form()
 
-class TestHandler(webapp2.RequestHandler):
     def post(self):
-        #q = self.request.get("q")
-        #self.response.out.write(q)
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
 
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(self.request)
+        month = valid_month(user_month)
+        day = valid_day(self.request.get(user_day))
+        year = valid_year(self.request.get(user_year))
+
+        if not (day and month and year):
+            self.write_form("Invalid date", user_month, user_day, user_year)
+        else: 
+            self.response.out.write("Thanks! That's a valid date.")
+
+
+months = ['january',
+          'february',
+          'march',
+          'april',
+          'may',
+          'june',
+          'july',
+          'august',
+          'september',
+          'october',
+          'november',
+          'december']
+#this does not work
+#def valid_month(month):
+#    if (months.index(month.lower()) != ValueError):
+#        return "January" #month.title()
+#    else:
+#        return "None"
+
+#make a dictionary
+#create a mapping of the first 3 letters of the month in lowercase and match that with the month
+
+month_abbvs = dict((m[:3].lower(), m) for m in months)
+#month_days = dict(for d in days, for m in months)
+
+#func with dictionary
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+
+#dumb version but it works 
+def valid_day(day):
+    if day.isdigit():
+        if int(day) <= 31 and int(day) > 0:
+            return int(day)
+
+def valid_year(year):
+    if year.isdigit():
+        if int(year) <= 2020 and int(year) > 1900:
+            return int(year)
 
 app = webapp2.WSGIApplication([
         ('/', MainPage),
-        ('/testform', TestHandler)
 ], debug=True)
---
+++ pythonNotes.py
@@ -0,0 +1,9 @@
+given_string = "I think that %s and %s are perfectly normal things to do in public"
+
+def sub_strings(s1, s2):
+    return givenString % (s1, s2)
+
+
+given_string2 = "I'm %(nickname)s. My real name is %(name)s, but my friends call me %(nickname)s."
+def sub_m(name, nickname):
+    
